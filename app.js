324dda256665d5ad73088ea450aa6903e639bb6d
Xeng Pha <69016286+phaxx031@users.noreply.github.com>
2020-11-14 12:53:07 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -25,13 +25,14 @@ const STORE = {
       correctAnswer: 'Mario'
     },
     {
-      question: 'What was the most profitable video game of all time to date?',
+      question: 'What is the most profitable video game of all time to date?',
       answers: [
         'Honor of Kings',
         'Monster Strike',
         'Fortnite',
         'Pokemon Go'
       ],
+      correctAnswer: 'Monster Strike'
     },
     {
       question: 'What is the average age of a gamer in the USA according to Wikipedia?',
@@ -40,7 +41,8 @@ const STORE = {
         '16',
         '35',
         '45'
-      ]
+      ],
+      correctAnswer: '35'
     },
     {
       question: 'How much was first place grand prize money at the 2019 Fortnite World Cup?',
@@ -49,7 +51,8 @@ const STORE = {
         '$3 million',
         '$5 million',
         '$10 million'
-      ]
+      ],
+      correctAnswer: '$3 million'
     }
   ],
   quizStarted: false,
@@ -57,23 +60,28 @@ const STORE = {
   score: 0
 };
 
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
 
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('')
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+$('#question').text(STORE.questions[0].question);
+$('#first_answer').text(STORE.questions[0].answers[0]);
+$('#second_answer').text(STORE.questions[0].answers[1]);
+$('#third_answer').text(STORE.questions[0].answers[2]);
+$('#fourth_answer').text(STORE.questions[0].answers[3]);
 
 let currentQuestionIndex
 
-startButton.addEventListener('click', startQuiz)
-
 function startQuiz() {
-  console.log('Started')
-  startButton.classList.add('hide')
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  $('#start-btn').click(function(event) {
+    console.log('Started');
+    $('#start-btn').hide();
+    $('.border').hide();
+    $('.hide').toggle();
+  });
 }
 
 function setNextQuestion() {
@@ -81,20 +89,23 @@ function setNextQuestion() {
   showQuestion(currentQuestionIndex)
 }
 
-function showQuestion(question)
+// the function below prolly needs to be altered since in the video he is using buttons and we are using radio 18:40
+function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
     const button = document.createElement('button')
     button.innerText = answer.text
     button.classList.add('btn')
-    if (answer.correct) {
+    // the problem/issue is below this
+    if (questions.correctAnswer == questions.correctAnswer) {
       button.dataset.correct = answer.correct
     }
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
   })
+}
 
-functionresetState() {
+function resetState() {
   nextButton.classList.add('hide')
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
@@ -107,6 +118,11 @@ function selectAnswer(e) {
   setStatusClass(document.body, correct)
 
 }
+
+function init() {
+  startQuiz();
+}
+$(init);
 
 /**
  * 
